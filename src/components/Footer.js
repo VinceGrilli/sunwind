bbe8b6f12253ce8e831d457d02d371027ce76943
BrainJarProductions <vgrilli1@gmail.com>
2020-10-06 12:43:59 -0400

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ import vimeo from '../img/social/vimeo.svg';
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className='footer has-background-white has-text-white-ter'>
+      <footer className='footer '>
         <div className='content has-text-centered'>
           <img
             src={logo}
@@ -21,48 +21,38 @@ const Footer = class extends React.Component {
         <div
           className='content has-text-centered has-text-white-ter'
           style={{
-            backgroundColor: '#2073af',
+            backgroundImage: 'linear-gradient(to right, #2073af , #3480b6)',
           }}
         >
-          <div
-            className='container has-text-white-ter'
-            style={{
-              backgroundColor: '#2073af',
-            }}
-          >
+          <div className='container has-text-white-ter'>
             <div style={{ maxWidth: '100vw' }} className='columns'>
               <div className='column is-4'>
                 <section className='menu'>
                   <ul className='menu-list'>
                     <li>
-                      <Link to='/' className='navbar-item'>
+                      <Link to='/' className='navbar-item wt'>
                         Home
                       </Link>
                     </li>
                     <li>
-                      <Link className='navbar-item' to='/about'>
+                      <Link className='navbar-item wt' to='/gallery'>
+                        Gallery
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className='navbar-item wt' to='/about'>
                         About
                       </Link>
                     </li>
                     <li>
-                      <Link className='navbar-item' to='/products'>
-                        Products
+                      <Link className='navbar-item wt' to='/blog'>
+                        Blog
                       </Link>
                     </li>
                     <li>
-                      <Link className='navbar-item' to='/contact/examples'>
-                        Form Examples
+                      <Link className='navbar-item wt' to='/wind'>
+                        Wind
                       </Link>
-                    </li>
-                    <li>
-                      <a
-                        className='navbar-item'
-                        href='/admin/'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                      >
-                        Admin
-                      </a>
                     </li>
                   </ul>
                 </section>
@@ -71,48 +61,77 @@ const Footer = class extends React.Component {
                 <section>
                   <ul className='menu-list'>
                     <li>
-                      <Link className='navbar-item' to='/blog'>
-                        Latest Stories
+                      <Link className='navbar-item wt' to='/solar'>
+                        Solar
                       </Link>
                     </li>
                     <li>
-                      <Link className='navbar-item' to='/contact'>
-                        Contact
+                      <Link
+                        className='navbar-item wt'
+                        to='/residential-commercial'
+                      >
+                        Residential & Commercial
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className='navbar-item wt' to='/incentives'>
+                        Incentive Programs
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className='navbar-item wt' to='/payment-options'>
+                        Payment Options
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className='navbar-item wt' to='/ground'>
+                        Ground-mount Systems
                       </Link>
                     </li>
                   </ul>
                 </section>
               </div>
-              <div className='column is-4 social'>
-                <a title='facebook' href='https://facebook.com'>
-                  <img
-                    src={facebook}
-                    alt='Facebook'
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title='twitter' href='https://twitter.com'>
-                  <img
-                    className='fas fa-lg'
-                    src={twitter}
-                    alt='Twitter'
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title='instagram' href='https://instagram.com'>
-                  <img
-                    src={instagram}
-                    alt='Instagram'
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title='vimeo' href='https://vimeo.com'>
-                  <img
-                    src={vimeo}
-                    alt='Vimeo'
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
+              <div className='column is-4'>
+                <ul className='menu-list'>
+                  <li>
+                    <Link
+                      className='navbar-item wt'
+                      to='/contact'
+                      style={{ textAlign: 'center' }}
+                    >
+                      Contact
+                    </Link>
+                  </li>
+                  <li>
+                    <a
+                      className='navbar-item wt'
+                      href='tel:508-258-7277"'
+                      style={{ textAlign: 'center' }}
+                    >
+                      508-258-7277
+                    </a>
+                  </li>
+                </ul>
+                <div className='social'>
+                  <a title='facebook' href='https://facebook.com'>
+                    <img
+                      src={facebook}
+                      alt='Facebook'
+                      style={{
+                        width: '1em',
+                        height: '1em',
+                      }}
+                    />
+                  </a>
+
+                  <a title='instagram' href='https://instagram.com'>
+                    <img
+                      src={instagram}
+                      alt='Instagram'
+                      style={{ width: '1em', height: '1em' }}
+                    />
+                  </a>
+                </div>
               </div>
             </div>
           </div>
